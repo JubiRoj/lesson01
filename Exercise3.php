e21c03586af78e84613089f19c6a16bdba10b523
Vsadnik <jubiroj@gmail.com>
2018-10-01 15:41:57 +0300

    Added class Worker, Manager

--- Exercise3.php
+++ Exercise3.php
@@ -23,19 +23,22 @@
 //Помимо организации самих классов, подготовить тестовое наполнение. Создать несколько человек, студентов, сотрудников, менеджеров.
 // Повызывать, написанные вами методы для соответсвующих объектов.
 //Дописать функционал, который обеспечит вам подсчет количества объектов каждого класса. Вывести эти количества в консоль.
-// Для тех, кто любит поинтереснее - в консоль вывести по каждому классу 2 числа (с учетом потомков и без учета потомков). Например, у нас 2 просто сотрудника и 3 менеджера. Для класса "Сотрудники" мы должны вывести "Сотрудники: 5/2"
+// Для тех, кто любит поинтереснее - в консоль вывести по каждому классу 2 числа (с учетом потомков и без учета потомков).
+// Например, у нас 2 просто сотрудника и 3 менеджера. Для класса "Сотрудники" мы должны вывести "Сотрудники: 5/2"
 //Подсчет количества объхектов каждого типа.
 //Примечание: вызов родительского метода и/или конструктора можно осуществялть через оператор parent::
 //
 include 'Human.php';
 
-$objHuman = new Human("Anton Jernov", 32);
+echo "\n\n===Test Human===========\n";
+$objHuman = new Human("Denis Jernov", 32);
 var_dump($objHuman);
 echo $objHuman->getFio() . PHP_EOL;
 $objHuman->setFio("Anatoliy Jernov");
 echo $objHuman->getFio() . PHP_EOL;
 echo $objHuman . PHP_EOL;
 
+echo "\n\n===Test Student=========\n";
 $student = new Student("Ivan Petrov", 18);
 $student->EnrollInCourse("1", Student::TYPE_OCHN);
 $student->addNumber(5);
@@ -46,7 +49,26 @@ var_dump($student);
 echo $student . PHP_EOL;
 echo $student->getListNumber() . PHP_EOL;
 
+echo "\n\n===Test Worker===========\n";
+$worker = new Worker("Andrey Stahanov", 29);
+$worker->setSalary(16);
+$worker->makePaid("10.07.1935","");
+$worker->makePaid("10.08.1935", 17);
+$worker->makePaid("31.08.1935", 220);
+echo $worker.PHP_EOL;
+echo $worker->getListPaid().PHP_EOL;
+//=========================
+
+echo "\n\n===Test Manager===========\n";
+$manager = new Manager("Nikolay Mashurov", 47);
+echo $manager.PHP_EOL;
+echo $manager->getListWorker();
+echo "Added worker...".PHP_EOL;
+$manager->addSubordination($worker);
+$manager->addSubordination(new Worker("Tihon Borisenko", 22));
+echo $manager->getListWorker();
+echo "\nDeleted worker...".PHP_EOL;
+$manager->delSubordination($worker);
+echo $manager->getListWorker();
 
 
-//$objStudent = new GoodStudent01();
-//$objStudent->setFirstName('Vasya');
